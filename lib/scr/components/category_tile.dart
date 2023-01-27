a80cd367247375008b15e7f7e63fca6159109f11
emersonbarina <emersonbarina@gmail.com>
2023-01-27 19:32:28 -0300

    Login UI - Home Tab - Category with ListView

---
+++ lib/scr/components/category_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:quitanda/scr/constants/colors.dart';
+import 'package:quitanda/scr/constants/sizes.dart';
+
+class CategoryTile extends StatelessWidget {
+  const CategoryTile({
+    Key? key,
+    required this.category,
+    required this.isSelected,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String category;
+  final bool isSelected;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(tBorderRadius),
+            color: isSelected ? tColorsPrimary : tColorsLightUnselected,
+          ),
+          child: Text(
+            category,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: isSelected ? tColorsLight : tColorsPrimary,
+              fontSize: isSelected ? 16 : 14,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/scr/home/home_tab.dart
+++ lib/scr/home/home_tab.dart
@@ -1,11 +1,29 @@
 import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:quitanda/scr/components/category_tile.dart';
 import 'package:quitanda/scr/constants/colors.dart';
 import 'package:quitanda/scr/constants/sizes.dart';
 import 'package:quitanda/scr/constants/texts.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  // Categories
+  List<String> categories = [
+    'Frutas',
+    'Verduras',
+    'Carnes',
+    'Bebidas',
+    'Biscoitos',
+    'Limpeza',
+  ];
+
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -88,6 +106,26 @@ class HomeTab extends StatelessWidget {
           ),
 
           // Categories
+          Container(
+            padding: const EdgeInsets.only(left: 25),
+            height: tHeightSizeBox,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = categories[index];
+                    });
+                  },
+                  category: categories[index],
+                  isSelected: categories[index] == selectedCategory,
+                );
+              },
+              separatorBuilder: (_, index) => const SizedBox(width: 10),
+              itemCount: categories.length,
+            ),
+          ),
 
           // Grid
         ],
