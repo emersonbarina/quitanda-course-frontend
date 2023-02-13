81e783f9752578672e98cbbd521ca6533b12a139
emersonbarina <emersonbarina@gmail.com>
2023-02-12 22:59:32 -0300

    Home - Using Binding (16-93)

--- lib/scr/page_route/app_pages.dart
+++ lib/scr/page_route/app_pages.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:quitanda/scr/pages/auth/view/sign_in_screen.dart';
 import 'package:quitanda/scr/pages/auth/view/sign_up_screen.dart';
 import 'package:quitanda/scr/pages/base/base_screen.dart';
+import 'package:quitanda/scr/pages/home/binding/hoem_binding.dart';
 import 'package:quitanda/scr/pages/splash/splash_screen.dart';
 
 abstract class AppPages {
@@ -10,29 +11,27 @@ abstract class AppPages {
       page: () => const SplashScreen(),
       name: PageRoutes.splashRoute,
     ),
-
     GetPage(
       page: () => SignInScreen(),
       name: PageRoutes.signInRoute,
     ),
-
     GetPage(
       page: () => SignUpScreen(),
       name: PageRoutes.signUpRoute,
     ),
-
     GetPage(
       page: () => const BaseScreen(),
       name: PageRoutes.baseRoute,
+      bindings: [
+        HomeBinding(),
+      ],
     ),
   ];
 }
 
-abstract class PageRoutes{
-
+abstract class PageRoutes {
   static const String signInRoute = '/signin';
   static const String signUpRoute = '/signup';
   static const String splashRoute = '/splash';
   static const String baseRoute = '/';
-
 }
