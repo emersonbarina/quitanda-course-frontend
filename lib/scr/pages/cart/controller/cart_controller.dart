2d4f6d231c918463c9cf88a319f262dac53e39b0
emersonbarina <emersonbarina@gmail.com>
2023-02-17 20:57:27 -0300

    Cart Items - load method when instance

--- lib/scr/pages/cart/controller/cart_controller.dart
+++ lib/scr/pages/cart/controller/cart_controller.dart
@@ -7,6 +7,12 @@ class CartController extends GetxController {
   final cartRepository = CartRepository();
   final authController = Get.find<AuthController>();
 
+  @override
+  void onInit(){
+    super.onInit();
+    getCartItems();
+  }
+
   Future<void> getCartItems() async {
     await cartRepository.getCartItems(
       token: authController.user.token!,
